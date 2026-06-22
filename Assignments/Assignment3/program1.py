64a8a30e9229c7f187470e4950f2ecbb0c9aab38
nidhikadival <nidhikadival11@gmail.com>
2026-06-22 20:05:40 +0530

    Added Assignment3 code

---
+++ Assignments/Assignment3/program1.py
@@ -0,0 +1,30 @@
+########################################################
+##  Name : Nidhi Girish Kadival
+##  Assignment 3
+##  Question 6
+##  Date : 21/06/2026
+########################################################
+
+import sys 
+
+print("Enter data : ")
+Data = input()
+
+print("Data type is : ",type(Data))
+print("Memory Address is : ",id(Data))
+print("Size is : ",sys.getsizeof(Data))
+
+########################################################
+## Output :
+## Enter data : 
+## Hello
+## Data type is :  <class 'str'>
+## Memory Address is :  4340504144
+## Size is :  46
+##
+## Enter data : 
+## 11
+## Data type is :  <class 'str'>
+## Memory Address is :  4309194320
+## Size is :  43
+########################################################
